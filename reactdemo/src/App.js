d49939c6fd0e6286c77f51805c0d4e11acc36af0
Vikky Singh <vikkyhp0007@gmail.com>
2022-03-11 23:57:22 +0530

    docker

--- reactdemo/src/App.js
+++ reactdemo/src/App.js
@@ -6,14 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hii there!!</p>
+        <p>hii there!!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn 🚀🚀🚀
         </a>
       </header>
     </div>
